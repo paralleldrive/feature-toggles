8b88990a035b63ea757dae2723af8c7c76370a24
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-23 17:13:32 -0700

    use ramda map

--- src/activate-features.js
+++ src/activate-features.js
@@ -2,6 +2,7 @@ import lensPath from 'ramda/src/lensPath';
 import set from 'ramda/src/set';
 import view from 'ramda/src/view';
 import curryN from 'ramda/src/curryN';
+import map from 'ramda/src/map';
 
 const nameLens = lensPath(['name']);
 const isActiveLens = lensPath(['isActive']);
@@ -13,10 +14,11 @@ const curryTwo = curryN(2);
 
 export const activateFeatures = curryTwo(
   (featureNamesToEnable = [], features = []) =>
-    features.map(
+    map(
       feature =>
         featureNamesToEnable.includes(getName(feature))
           ? setIsActive(true, feature)
-          : feature
+          : feature,
+      features
     )
 );
